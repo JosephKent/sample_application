5306470388b0cb50d66b97c5d87275750a739ebf
josephkent <joseph@contractiq.in>
2016-08-17 11:49:23 +0530

    Finish layout and routes

--- Gemfile
+++ Gemfile
@@ -2,6 +2,8 @@ source 'https://rubygems.org'
 
 gem 'rails',        '4.0.0'
 
+gem 'bootstrap-sass', '3.2.0.2'
+
 gem 'sqlite3'
 
 gem 'puma',         '3.4.0'
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 SampleApplication::Application.routes.draw do
+
   root 'static_pages#home'
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
-  get "static_pages/contact"
+  get  '/help', to: 'static_pages#help'
+  get  '/about', to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
+  get  '/signup', to: 'users#new'
   #root 'application#hello'
 
   # Example of regular route:
